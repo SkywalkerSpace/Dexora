e3e18e737f834c8b7d252b0b2ddb5fbe77c39da2
SkywalkerSpace <dzmbbs@qq.com>
2026-08-17 21:56:25 +0800

    available_models

--- models/multimodal_encoder/t5_encoder.py
+++ models/multimodal_encoder/t5_encoder.py
@@ -7,6 +7,7 @@ class T5Embedder:
         "/home/ubuntu/myh/experiment/Dexora/google/t5-v1_1-small",
         "/home/ubuntu/myh/experiment/Dexora/google/t5-v1_1-base",
         "/home/ubuntu/myh/experiment/Dexora/google/t5-v1_1-large",
+        "/home/mayuhang/models/t5-v1_1-large",
         "google/t5-v1_1-small",
         "google/t5-v1_1-base",
         "google/t5-v1_1-large",
